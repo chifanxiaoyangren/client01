99f65154fe280ee7a4bbdf1c7649ad0a751a5f8f
dong <964158490@qq.com>
2020-06-30 15:13:17 +0800

    first commit

--- WindowsFormsApp29/Form1.cs
+++ WindowsFormsApp29/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +16,41 @@ namespace WindowsFormsApp29
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string sendGet()
+        {
+            byte [] b=Encoding.Default.GetBytes("GET");
+            socket.Send(b);
+            byte[] re = new byte[1024];
+            socket.Receive(re);
+
+            return Encoding.Default.GetString(re).Trim();
+        }
+
+
+        public static Socket socket;
+        public string ConnectServer()
+        {
+            socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("10.1.136.32"), 10086));
+            byte[] re = new byte[1024];
+            socket.Receive(re);
+            string s = Encoding.Default.GetString(re).Trim().TrimEnd().Replace("\0","");
+            return s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           if( ConnectServer()=="Login")
+            {
+                MessageBox.Show("登录成功");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(sendGet());
         }
     }
 }
